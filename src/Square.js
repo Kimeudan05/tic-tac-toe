53ab208746b3706444b424fcb0666dab790891f1
kimeu <kimeudan05@gmail.com>
2024-09-11 13:17:56 +0300

    deleted some files

--- src/Square.js
+++ src/Square.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Square = ({ value, onSquareClick }) => {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className={`square ${
+        value === "X" ? "x-value" : value === "O" ? "o-value" : ""
+      }`}
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
